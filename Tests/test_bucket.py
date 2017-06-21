477e031cbdb67d34aa133c33c5845fce7dfa1c86
sarahmbaka <salliedailey@gmail.com>
2017-06-21 12:45:03 +0300

    [TEST] add bucket test

---
+++ Tests/test_bucket.py
@@ -0,0 +1,25 @@
+import unittest
+from app.views import View
+
+class Test_bucket(unittest.TestCase):
+    """
+    This calss tests Bucket functionality
+    """
+    def setUp(self):
+        """setup"""
+        self.auth = View()
+
+    def test_bucket_add(self):
+        """
+        Tests for successful bucket add
+        """
+        response = self.auth.add_bucket('travel', 'My travel wish list')
+        self.assertEqual('You have successfully added a bucket', response)
+
+    
+
+    def tearDown(self):
+        del self.auth
+
+if __name__ == '__main__':
+    unittest.main()
