12a2d3d98f7b2b41aa9ac4e4f14108d582634cc2
sarahmbaka <salliedailey@gmail.com>
2017-06-21 12:46:46 +0300

    [TEST]  Add delete bucket test

--- Tests/test_bucket.py
+++ Tests/test_bucket.py
@@ -16,6 +16,13 @@ class Test_bucket(unittest.TestCase):
         response = self.auth.add_bucket('travel', 'My travel wish list')
         self.assertEqual('You have successfully added a bucket', response)
 
+    def test_bucket_delete(self):
+        """
+        Tests for successful delete of item
+        """
+        response = self.auth.del_bucket('travel')
+        self.assertEqual('Bucket deleted', response)
+
     
 
     def tearDown(self):
